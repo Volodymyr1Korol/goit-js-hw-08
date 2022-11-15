9b0652c0a86d232da19d589cf0868c016fb9bb69
Volodymyr1Korol <volodymyrkorol18@gmail.com>
2022-11-15 21:58:46 +0200

    8HW

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('.feedback-form');
+const {
+    elements: { email, message }
+} = form;
+const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {
+    email: '',
+    message: '',
+};
+const _ = require('underscore');
 
+form.addEventListener('input', _.throttle(saveData, 500));
+form.addEventListener('submit', formSubmit);
+
+email.value = formData.email;
+message.value = formData.message;
+
+function saveData() {
+    localStorage.setItem(
+        'feedback-form-state',
+        JSON.stringify({ email: email.value, message: message.value })
+    );
+}
+
+function formSubmit(event) {
+    event.preventDefault();
+    console.log(localStorage.getItem('feedback-form-state'));
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+}
